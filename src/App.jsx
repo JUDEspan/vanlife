c0ad134577d3ed695f3e27b836c500b4eb68a7ad
Odede Jude <judeodede@gmail.com>
2024-02-28 11:00:56 +0100

    first

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,30 @@ function App() {
 
   return (
     <>
-    <Navbar />
-    <SignUp/>
+   <Routes>
+  <Route path="/" element={<Layout/>}>
+    <Route path="/" element={<Home/>} />
+    <Route path="*" element={<NotFound/>} />
+    <Route path="/home" element={<Navigate to="/" />} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/vans" element={<Vandetails/>}>
+    </Route>
+      <Route path="/vans/:id" element={<VanDescript/>} />
+
+
+
+
+
+<Route path='/host' element={<Host/>}>
+<Route path='/host' element={<Navigate to='/host/dashboard'/>} />
+    <Route path='/host/dashboard' element={<HostDash/>} />
+    <Route path='/host/review' element={<HostReview/>} />
+    <Route path='/host/income' element={<HostIncome/>} />
+    <Route path='/host/van' element={<HostVans/>} />
+</Route>
+  </Route>
+
+</Routes>
     </>
   )
 }
